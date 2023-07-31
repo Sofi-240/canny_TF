760f1a07e737ae238a2900148c0ad798a3a55912
Sofi-240 <54475550+Sofi-240@users.noreply.github.com>
2023-07-31 13:16:34 +0300

    layer

--- canny.py
+++ canny.py
@@ -5,92 +5,6 @@ PI = tf.cast(
 )
 
 
-def gaussian_setup(kernel_size, sigma=None):
-    if sigma is None:
-        sigma = 0.3 * ((kernel_size - 1) * 0.5 - 1) + 0.8
-
-    ax = tf.range(-kernel_size // 2 + 1.0, kernel_size // 2 + 1.0)
-    xx, yy = tf.meshgrid(ax, ax)
-    normal = 1 / (2.0 * PI * (sigma ** 2))
-    kernel = tf.exp(
-        -((xx ** 2) + (yy ** 2)) / (2.0 * (sigma ** 2))
-    ) * normal
-
-    kernel = kernel / tf.reduce_sum(kernel)
-
-    out = tf.reshape(
-        kernel, shape=(kernel_size, kernel_size, 1, 1), name='gaussian_kernel'
-    )
-    return out
-
-
-def sobel_setup():
-    h = tf.reshape(
-        tf.constant(
-            [[-1, 0, 1],
-             [-2, 0, 2],
-             [-1, 0, 1]], tf.float32
-        ), shape=(3, 3, 1, 1), name='H'
-    )
-    v = tf.reshape(
-        tf.constant(
-            [[-1, -2, -1],
-             [0, 0, 0],
-             [1, 2, 1]], tf.float32
-        ), shape=(3, 3, 1, 1), name='V'
-    )
-    out = tf.concat(
-        (h, v), axis=-1, name='sobel_kernel'
-    )
-    return out
-
-
-def localmax_setup():
-    ang_0_kernel = tf.reshape(
-        tf.constant(
-            [
-                [-float('inf'), -float('inf'), -float('inf')],
-                [0.0, 0.0, 0.0],
-                [-float('inf'), -float('inf'), -float('inf')]
-            ], dtype=tf.float32
-        ), shape=(3, 3, 1), name='ang_0_kernel'
-    )
-    ang_45_kernel = tf.reshape(
-        tf.constant(
-            [
-                [-float('inf'), -float('inf'), 0.0],
-                [-float('inf'), 0.0, -float('inf')],
-                [0.0, -float('inf'), -float('inf')]
-            ], dtype=tf.float32
-        ), shape=(3, 3, 1), name='ang_45_kernel'
-    )
-    ang_90_kernel = tf.reshape(
-        tf.constant(
-            [
-                [-float('inf'), 0.0, -float('inf')],
-                [-float('inf'), 0.0, -float('inf')],
-                [-float('inf'), 0.0, -float('inf')]
-            ], dtype=tf.float32
-        ), shape=(3, 3, 1), name='ang_90_kernel'
-    )
-    ang_135_kernel = tf.reshape(
-        tf.constant(
-            [
-                [0.0, -float('inf'), -float('inf')],
-                [-float('inf'), 0.0, -float('inf')],
-                [-float('inf'), -float('inf'), 0.0]
-            ], dtype=tf.float32
-        ), shape=(3, 3, 1), name='ang_90_kernel'
-    )
-    angles_setup = [
-        (tf.math.logical_or, (157.5, 22.5), ang_0_kernel),
-        (tf.math.logical_and, (22.5, 67.5), ang_45_kernel),
-        (tf.math.logical_and, (67.5, 112.5), ang_90_kernel),
-        (tf.math.logical_and, (112.5, 157.5), ang_135_kernel)
-    ]
-    return angles_setup
-
-
 def pad(images, b=None, h=None, w=None, d=None, **kwargs):
     paddings = []
     for arg in [b, h, w, d]:
@@ -106,55 +20,106 @@ def pad(images, b=None, h=None, w=None, d=None, **kwargs):
     return padded
 
 
-@tf.function
-def canny_edge(images, sigma=None, kernel_size=5, min_val=50, max_val=100, connection_iterations=20):
-    X = tf.identity(images, name='X') if tf.is_tensor(images) else tf.convert_to_tensor(images, name='X')
-    n_dim = len(X.shape)
-    if n_dim < 2:
-        raise ValueError(
-            f'expected for 2/3/4D tensor but got {n_dim} tensor'
-        )
-    X = tf.expand_dims(X, axis=-1, name='X') if n_dim == 2 else X
-    X = tf.expand_dims(X, axis=0, name='X') if n_dim <= 3 else X
+def noise_reduction(X, kernel_size, sigma=None):
+    if sigma is None:
+        sigma = 0.3 * ((kernel_size - 1) * 0.5 - 1) + 0.8
+    ax = tf.range(-kernel_size // 2 + 1.0, kernel_size // 2 + 1.0)
+    xx, yy = tf.meshgrid(ax, ax)
+    normal = 1 / (2.0 * PI * (sigma ** 2))
+    kernel = tf.exp(
+        -((xx ** 2) + (yy ** 2)) / (2.0 * (sigma ** 2))
+    ) * normal
 
-    X = tf.cast(X, dtype=tf.float32, name='X')
+    kernel = kernel / tf.reduce_sum(kernel)
 
-    gaussian_kernel = gaussian_setup(kernel_size, sigma)
-    sobel_kernel = sobel_setup()
-    angle_cond_rng_kernel = localmax_setup()
-    hysteresis_kernel = tf.ones(shape=(5, 5, 1), dtype=tf.float32)
+    kernel = tf.reshape(
+        kernel, shape=(kernel_size, kernel_size, 1, 1), name='gaussian_kernel'
+    )
+    with tf.name_scope('noise_reduction'):
+        X_pad = pad(X, h=kernel_size // 2, w=kernel_size // 2, mode='SYMMETRIC', name='X_pad')
+        Xg = tf.nn.convolution(X_pad, kernel, padding='VALID', name='Xg')
+        return Xg
 
-    with tf.name_scope('canny_edge'):
-        Xg = tf.nn.convolution(
-            pad(X, h=kernel_size // 2, w=kernel_size // 2, mode='SYMMETRIC', name='X_pad'),
-            gaussian_kernel, padding='VALID', name='Xg'
-        )
 
-        Gxy = tf.nn.convolution(
-            pad(Xg, h=1, w=1, constant_values=0.0, name='Xg_pad'),
-            sobel_kernel, padding='VALID', name='Gxy'
-        )
-
+def gradient_calculation(X):
+    sobel_kernel = tf.constant(
+        [
+            [[[-1, -1]], [[0, -2]], [[1, -1]]],
+            [[[-2, 0]], [[0, 0]], [[2, 0]]],
+            [[[-1, 1]], [[0, 2]], [[1, 1]]]
+        ], dtype=tf.float32, name='sobel_kernel'
+    )
+    with tf.name_scope('gradient_calculation'):
+        X_pad = pad(X, h=1, w=1, constant_values=0.0, name='X_pad')
+        Gxy = tf.nn.convolution(X_pad, sobel_kernel, padding='VALID', name='Gxy')
         gx, gy = tf.split(Gxy, [1, 1], axis=-1)
 
         theta = ((tf.atan2(gx, gy, name='theta') * 180 / PI) + 90) % 180
         Gxy = tf.sqrt((gx ** 2) + (gy ** 2), name='Gxy')
-        edge_before_thresh = None
+        return Gxy, theta
 
-        angles_setup = [
-            (tf.math.logical_or, (157.5, 22.5), ang_0_kernel),
-            (tf.math.logical_and, (22.5, 67.5), ang_45_kernel),
-            (tf.math.logical_and, (67.5, 112.5), ang_90_kernel),
-            (tf.math.logical_and, (112.5, 157.5), ang_135_kernel)
-        ]
 
-        for call_, rng, kernel in angle_cond_rng_kernel:
-            ang_image = tf.where(
-                call_(
-                    tf.math.greater_equal(theta, rng[0]),
-                    tf.math.less(theta, rng[1])
-                ), Gxy, 0.0
-            )
+def non_maximum_suppression(Gxy, theta):
+    ang_0_kernel = tf.constant(
+        [
+            [[-float('inf')], [-float('inf')], [-float('inf')]],
+            [[0.0], [0.0], [0.0]],
+            [[-float('inf')], [-float('inf')], [-float('inf')]]
+        ], dtype=tf.float32, name='ang_0_kernel'
+    )
+    ang_45_kernel = tf.constant(
+        [
+            [[-float('inf')], [-float('inf')], [0.0]],
+            [[-float('inf')], [0.0], [-float('inf')]],
+            [[0.0], [-float('inf')], [-float('inf')]]
+        ], dtype=tf.float32, name='ang_45_kernel'
+    )
+    ang_90_kernel = tf.constant(
+        [
+            [[-float('inf')], [0.0], [-float('inf')]],
+            [[-float('inf')], [0.0], [-float('inf')]],
+            [[-float('inf')], [0.0], [-float('inf')]]
+        ], dtype=tf.float32, name='ang_90_kernel'
+    )
+    ang_135_kernel = tf.constant(
+        [
+            [[0.0], [-float('inf')], [-float('inf')]],
+            [[-float('inf')], [0.0], [-float('inf')]],
+            [[-float('inf')], [-float('inf')], [0.0]]
+        ], dtype=tf.float32, name='ang_90_kernel'
+    )
+    angle_kernel = tf.concat(
+        [tf.expand_dims(kernel, axis=0) for kernel in \
+         [ang_0_kernel, ang_45_kernel, ang_90_kernel, ang_135_kernel]],
+        axis=0, name='angle_kernel'
+    )
+    angle_rng = tf.constant(
+        [[157.5, 22.5],
+         [22.5, 67.5],
+         [67.5, 112.5],
+         [112.5, 157.5]], dtype=tf.float32, name='angle_rng'
+    )
+
+    edge_before_thresh = tf.zeros_like(Gxy, name='edge_before_thresh')
+
+    with tf.name_scope('non_maximum_suppression'):
+        for i in tf.range(4):
+            kernel = angle_kernel[i, ...]
+            rng = angle_rng[i]
+            if i == 0:
+                ang_image = tf.where(
+                    tf.math.logical_or(
+                        tf.math.greater_equal(theta, rng[0]),
+                        tf.math.less_equal(theta, rng[1])
+                    ), Gxy, 0.0
+                )
+            else:
+                ang_image = tf.where(
+                    tf.math.logical_and(
+                        tf.math.greater_equal(theta, rng[0]),
+                        tf.math.less(theta, rng[1])
+                    ), Gxy, 0.0
+                )
             max_pool_ang = tf.nn.dilation2d(
                 pad(ang_image, h=1, w=1, constant_values=0.0),
                 kernel, (1, 1, 1, 1), 'VALID', 'NHWC', (1, 1, 1, 1)
@@ -163,24 +128,39 @@ def canny_edge(images, sigma=None, kernel_size=5, min_val=50, max_val=100, conne
             is_max = tf.where(
                 tf.math.equal(max_pool_ang, ang_image), ang_image, 0.0
             )
-            if edge_before_thresh is None:
-                edge_before_thresh = is_max
-            else:
-                edge_before_thresh = edge_before_thresh + is_max
+            edge_before_thresh = edge_before_thresh + is_max
+        return edge_before_thresh
 
+
+def double_thresholding(X, min_val=50, max_val=100):
+    with tf.name_scope('double_thresholding'):
         edge_sure = tf.where(
-            tf.math.greater_equal(edge_before_thresh, max_val), 1.0, 0.0, name='edge_sure'
+            tf.math.greater_equal(X, max_val), 1.0, 0.0, name='edge_sure'
         )
         edge_week = tf.where(
             tf.math.logical_and(
-                tf.math.greater_equal(edge_before_thresh, min_val),
-                tf.math.less(edge_before_thresh, max_val)
+                tf.math.greater_equal(X, min_val),
+                tf.math.less(X, max_val)
             ), 1.0, 0.0, name='edge_week'
         )
+        return edge_sure, edge_week
 
-        connected = tf.add(edge_sure, edge_week, name='connected')
 
-        for _ in tf.range(connection_iterations):
+def hysteresis_tracking(edge_sure, edge_week, iterations=20, alg='connected'):
+    with tf.name_scope('hysteresis_tracking'):
+        if alg == 'connected':
+            return connected_alg(edge_sure, edge_week, iterations)
+        else:
+            return None
+
+
+def connected_alg(edge_sure, edge_week, iterations=20):
+    hysteresis_kernel = tf.ones(shape=(5, 5, 1), dtype=tf.float32)
+
+    connected = tf.add(edge_sure, edge_week, name='connected')
+    # TODO: bw open?  ---> dilation2d(erosion2d + edge) - edge ---> where >= 1 else 0
+    with tf.name_scope('connected_alg'):
+        for _ in tf.range(iterations):
 
             prev_connected = tf.identity(connected, name='prev_connected')
             connected = tf.nn.dilation2d(
@@ -201,4 +181,36 @@ def canny_edge(images, sigma=None, kernel_size=5, min_val=50, max_val=100, conne
                 connected + edge_sure, 1.0
             ), 255.0, 0.0, name='edge'
         )
+
         return edge
+
+
+@tf.function
+def canny_edge(images, sigma=None, kernel_size=5,
+               min_val=50, max_val=100, hysteresis_alg='connected', connection_iterations=20):
+    X = tf.identity(images, name='X') if tf.is_tensor(images) else tf.convert_to_tensor(images, name='X')
+    n_dim = len(X.shape)
+    if n_dim < 2:
+        raise ValueError(
+            f'expected for 2/3/4D tensor but got {n_dim} tensor'
+        )
+    X = tf.expand_dims(X, axis=-1, name='X') if n_dim == 2 else X
+    X = tf.expand_dims(X, axis=0, name='X') if n_dim <= 3 else X
+
+    X = tf.cast(X, dtype=tf.float32, name='X')
+    with tf.name_scope('canny_edge'):
+        Xg = noise_reduction(
+            X, kernel_size=kernel_size, sigma=sigma
+        )
+        Gxy, theta = gradient_calculation(Xg)
+        edge_before_thresh = non_maximum_suppression(
+            Gxy, theta
+        )
+        edge_sure, edge_week = double_thresholding(
+            edge_before_thresh, min_val=min_val, max_val=max_val
+        )
+        edge = hysteresis_tracking(
+            edge_sure, edge_week, iterations=connection_iterations, alg=hysteresis_alg
+        )
+        return edge
+
--- show_test.py
+++ show_test.py
@@ -2,22 +2,33 @@ import cv2
 from matplotlib import pyplot as plt
 from canny import canny_edge
 import matplotlib
+import tensorflow as tf
+import numpy as np
 
 matplotlib.use("Qt5Agg")
 
-image = cv2.imread('test_img.jpg')
-image = cv2.cvtColor(
-    image, cv2.COLOR_BGR2GRAY
-)
-edges_opencv = cv2.Canny(
-    image, 50, 100
-)
 
-edge_tf = canny_edge(image, max_val=100, min_val=50, connection_iterations=5, sigma=0.5)
+def show_images(images, subplot_x=1, subplot_y=1):
+    def show(image, ax):
+        if tf.is_tensor(image):
+            image = image.numpy()
+        if np.max(image) > 1:
+            image = image.astype(np.uint8)
+        if len(image.shape) == 2 or image.shape[-1] == 1:
+            ax.imshow(image, cmap='gray')
+        else:
+            ax.imshow(image)
+
+    fig, _ = plt.subplots(subplot_y, subplot_x, subplot_kw={'xticks': [], 'yticks': []})
+    fig.subplots_adjust(wspace=0, hspace=0)
+    for i in range(subplot_x * subplot_y):
+        show(images[i], fig.axes[i])
 
 
-fig, ax = plt.subplots(1, 3, subplot_kw={'xticks': [], 'yticks': []})
-fig.subplots_adjust(wspace=0)
-ax[0].imshow(image, cmap='gray')
-ax[1].imshow(edges_opencv, cmap='gray')
-ax[2].imshow(edge_tf.numpy()[0].astype('uint8'), cmap='gray')
+img = cv2.imread('test_img.jpg', 0)
+
+edges_opencv = cv2.Canny(img, 50, 100)
+
+edge_tf = canny_edge(img, max_val=100, min_val=50, connection_iterations=5, sigma=0.5)
+
+show_images([edges_opencv, edge_tf[0, ...]], subplot_x=2, subplot_y=1)
